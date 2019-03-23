2c49750b83e191307fa827da3739f39d762a4ff4
acrylic-style <me@acrylicstyle.xyz>
2019-03-24 03:22:59 +0900

    please note start.js is testing

--- cmds/start.js
+++ cmds/start.js
@@ -1,105 +1,92 @@
+const { LoggerFactory } = require('logger.js');
+const logger = LoggerFactory.getLogger('start', 'cyan');
 const Discord = require('discord.js');
 const Listing = require('./../modules/Listing');
 
+module.exports.run = async (bot, message) => {
+    const filter = user => !user.author.bot;
+    const game = new Listing();
 
-module.exports.run = async (bot, message, args,) => {
-
-    let snipeChannel = message.channel;
-    const filter = m => !m.author.bot;
-    let game = new Listing();
-
-    let editLast2 = null;
-
+    /* eslint-disable
     //let startMessage = new Discord.RichEmbed()
         //.setTitle("Fortnite scrim Bot")
         //.setDescription("試合開始後、左上に出るマッチコードの下３桁を書いてください")
         //.setColor("#019dff")
         //.setFooter("FsB");
+    */
 
     //message.channel.send({embed: startMessage});
 
-    console.log(`コマンドを検知：!sbs1 - 実行ユーザー：${message.author.username}`)
-
-    let last31 = new Discord.RichEmbed()
-        .setTitle("プレイヤー一覧 - 合計 0 人")
+    const players = {};
+    const msg_players = {};
+    players[message.guild.id] = new Discord.RichEmbed()
+        .setTitle(`プレイヤー一覧 - 合計 0 人`)
         .setColor("1af216");
+    msg_players[message.guild.id] = await message.channel.send(players); // eslint-disable-line
 
-    setTimeout(async () => {
-        editLast31 = await message.channel.send({embed: last31});  
-    }, 10);
+    logger.info(`コマンドを検知：!sbs - 実行ユーザー：${message.author.username}`);
 
-    const collector = snipeChannel.createMessageCollector(filter, {max: 200, maxMatches: 200, time: 180000});
-
-    collector.on('collect', m => {
-        console.log(`Collected ${m.content} | ${m.author.username}`);
+    const collector = {};
+    collector[message.guild.id] = message.channel.createMessageCollector(filter, {max: 200, maxMatches: 200, time: 180000});
+    collector[message.guild.id].on('collect', m => {
+        logger.info(`Collected ${m.content} | ${m.author.username}`);
         if (game.data.length === 0 && m.content.length === 3){
             game.addID(m.content.toUpperCase(), m.author.username);
-        }else if (m.content.length === 3){
+        } else if (m.content.length === 3){
             if (game.userPresent(m.author.username)){
                 game.deleteUserEntry(m.author.username);
                 if (game.idPresent(m.content.toUpperCase())){
                     game.addUser(m.content.toUpperCase(), m.author.username);
-                }else {
+                } else {
                     game.addID(m.content.toUpperCase(),m.author.username);
                 }
             } else {
                 if (game.idPresent(m.content.toUpperCase())){
                     game.addUser(m.content.toUpperCase(), m.author.username);
-                }else {
+                } else {
                     game.addID(m.content.toUpperCase(), m.author.username);
                 }
             }
         }
-
         game.sort();
-
         // 下三桁を認識するまでのところ
         let str = "";
-        last31 = new Discord.RichEmbed()
+        players[message.guild.id] = new Discord.RichEmbed()
             .setTitle(`プレイヤー一覧 - 合計 ${game.users.length} 人`)
             .setColor("#1af216");
+        logger.info(`コマンドを検知：!sbs - 実行ユーザー：${message.author.username}`);
 
-        
-
-        for (var i = 0; i < game.data.length; i++){ 
-
+        for (let i = 0; i < game.data.length; i++){
             str = "";
-            for (var j = 0; j < game.data[i].users.length ; j++){
-                str += `${game.data[i].users[j]}` + "\n";
+            for (let j = 0; j < game.data[i].users.length ; j++){
+                str += game.data[i].users[j] + "\n";
             }
-            last31.addField(`ID: ${game.data[i].id.toLowerCase()} - ${game.data[i].users.length}人`, str, true);
+            players[message.guild.id].addField(`ID: ${game.data[i].id.toLowerCase()} - ${game.data[i].users.length}人`, str, true);
         }
 
-        editLast31.edit({embed: last31}).catch((err) => {
-            console.log("Caught edit error");
+        msg_players[message.guild.id].edit(players[message.guild.id]).catch(err => {
+            logger.error(`Error while editing message: ${err}`);
         });
 
-            if (m.deletable){
-                m.delete().catch((err) => {
-                    console.log("Cant delete");
-                    console.log(err);
-                });
-            }
-
+        if (m.deletable){
+            m.delete().catch((err) => {
+                logger.error(`Unable to delete message: ${err}`);
+            });
+        }
     });
 
-    collector.on('end', collected => {
-        let chatlocked = new Discord.RichEmbed()
+    collector[message.guild.id].on('end', collected => {
+        const chatlocked = new Discord.RichEmbed()
             .setTitle("マッチコードの入力受け付けを締め切りました")
             .setDescription(`今回のマッチ参加者は ${game.users.length} 人です。\n\n頑張ってください！Good luck！`)
             .setColor("#ff0000");
-        console.log(`[CHAT LOCKED]Collected ${collected.size} items`);
-
-        message.channel.send({embed: chatlocked});
-
-
+        logger.info(`[CHAT LOCKED] Collected ${collected.size} items`);
+        message.channel.send(chatlocked);
     });
-
-
-}
+};
 
 
 
 module.exports.help = {
-    name: "sbs1"
-}
+    name: "sbs",
+};
